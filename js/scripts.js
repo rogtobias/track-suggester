011620dcbe3f2ac8d27b6ea0c9acf5bc0a6a9445
Roger Tobias <celticspyder14@gmail.com>
2017-04-28 12:20:27 -0700

    This commit will add the second question for the user to answer. It was checked and the variable is being populated with all of the options.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var userInput = $("input#user").val();
     var peoplePerson = $("#peoplePerson").val();
+    var personalityType = $("#personalityType").val();
     debugger;
     $(".user").append(userInput);
 
