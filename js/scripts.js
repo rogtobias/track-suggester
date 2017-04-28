f07fc73cc323de9c0d7366249da550653d322d9e
Roger Tobias <celticspyder14@gmail.com>
2017-04-28 11:41:18 -0700

    This commit will add the first question for the user to answer. It was checked and the variable is being populated with both options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var userInput = $("input#user").val();
-
+    var peoplePerson = $("#peoplePerson").val();
+    debugger;
     $(".user").append(userInput);
 
     $("#recommendationStory").show();
